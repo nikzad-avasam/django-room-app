a5967e291df3ad40c2a983e01d80a437a440a4c1
nikzad-avasam <abbas.nikzad70@gmail.com>
2024-04-26 14:23:04 +0330

    update room source code Django

--- merkit/base/forms.py
+++ merkit/base/forms.py
@@ -1,5 +1,11 @@
 from django.forms import ModelForm
 from .models import Room , User
+from django.contrib.auth.forms import UserCreationForm
+
+class newUserCreationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username','email','name','password1','password2']
 
 class RoomForm(ModelForm):
     class Meta:
@@ -10,4 +16,4 @@ class RoomForm(ModelForm):
 class UserForm(ModelForm):
     class Meta:
         model = User
-        fields = ['username','email']
+        fields = ['username','email','name','profile_image','bio']
--- merkit/base/views.py
+++ merkit/base/views.py
@@ -6,10 +6,10 @@ from django.contrib import messages
 from django.contrib.auth import authenticate,login,logout
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
-from django.contrib.auth.forms import UserCreationForm
+from .forms import newUserCreationForm
 
 def registerPage(request):
-    form = UserCreationForm(request.POST)
+    form = newUserCreationForm(request.POST)
     if request.method == 'POST':
         if form.is_valid():
             user = form.save(commit=False)
@@ -28,14 +28,14 @@ def loginPage(request):
         return redirect('home')
     
     if request.method == 'POST':
-        username = request.POST.get('username').lower()
+        email = request.POST.get('email').lower()
         password = request.POST.get('password')
         try:
-            user = User.objects.get(username=username)
+            user = User.objects.get(email=email)
         except:
             messages.error(request, "User not found.")
         
-        user = authenticate(request , username=username,password=password)
+        user = authenticate(request , email=email,password=password)
         if user is not None : 
             login(request , user)
             return redirect('home')
@@ -156,7 +156,7 @@ def updateUser(request):
     user = request.user
     form = UserForm(instance=user)
     if request.method == 'POST':
-        form = UserForm(request.POST,instance = user)
+        form = UserForm(request.POST,request.FILES,instance = user)
         if form.is_valid():
             form.save()
             return redirect('user-profile',pk=user.id)
